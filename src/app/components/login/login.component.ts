640faf8fa8c8c0d23b3d932eae268ae07c5cf6fe
Cesar O <omar18pe@gmail.com>
2019-01-18 00:39:28 -0500

    se agrego la autenticacion con google, twitter, y se pulio algunos detalles para que los mensajes se muestren diferenciados de acuerdo al usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component} from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,12 @@ import { Component} from '@angular/core';
 })
 export class LoginComponent  {
 
-  constructor() { }
+  constructor(public _cs: ChatService) { }
 
 ingresar( proveedor: string ) {
         console.log( proveedor);
+
+      this._cs.login( proveedor);
 }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Mensaje } from '../interface/mensaje.interface';
 import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
 
 
 @Injectable()
@@ -11,9 +14,41 @@ export class ChatService {
   private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
   public chats: Mensaje [] = [];
+  public usuario: any = {};
 
-  constructor( private afs: AngularFirestore ) { }
+  constructor( private afs: AngularFirestore, public afAuth: AngularFireAuth ) { 
 
+    this.afAuth.authState.subscribe( user => {
+        console.log('Estado del usuario: ', user);
+        if ( !user) {
+          return;
+        }
+        this.usuario.nombre = user.displayName;
+        this.usuario.uid = user.uid;
+
+    });
+
+  }
+
+  login( proveedor: string ) {
+
+      if (proveedor === 'google') {
+
+        this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+
+      } else {
+
+        this.afAuth.auth.signInWithPopup(new auth.TwitterAuthProvider());
+
+      }
+
+  }
+
+  logout() {
+    this.usuario = {};
+    this.afAuth.auth.signOut();
+
+  }
 
   cargarMensajes() {
     this.itemsCollection = this.afs.collection<Mensaje>('chats', ref => ref.orderBy('fecha', 'desc')
@@ -36,9 +71,10 @@ export class ChatService {
 agregarMensaje( texto: string) {
 
   const mensaje: Mensaje = {
-nombre: 'cesar',
+nombre: this.usuario.nombre,
 mensaje: texto,
-fecha: new Date().getTime()
+fecha: new Date().getTime(),
+uid: this.usuario.uid
   };
 
    return this.itemsCollection.add( mensaje);
